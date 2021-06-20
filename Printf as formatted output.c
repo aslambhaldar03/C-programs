f481dd12776b6c42f16ff7f8044eb6182066b64e
aslambhaldar03 <86196631+aslambhaldar03@users.noreply.github.com>
2021-06-20 08:31:39 -0700

    Basic C Programs

---
+++ Printf as formatted output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  printf("\n %4d %-20s %3d %3d %3d %3d %6.2f",255,"Amar Chavan",100,100,100,300,100.00);
+  printf("\n %4d %-20s %3d %3d %3d %3d %6.2f",2,"Raj Mohite",95,35,70,200,66.666);
+  printf("\n %4d %-20s %3d %3d %3d %3d %6.2f",152,"Sahil Mane",78,52,90,170,56.7424);
+
+  getch();
+  return 0;
+
+}
