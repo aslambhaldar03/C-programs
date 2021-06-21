70dad3db1e2daa6815a65d4d51f1ddca396f4928
aslambhaldar03 <86196631+aslambhaldar03@users.noreply.github.com>
2021-06-21 14:43:04 +0530

    Add files via upload

---
+++ ASCII Value to Its Equivalent Character.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Val = 0;
+
+    printf("\n Enter an ASCII Value To get Correspondig Character = ");
+    scanf("%d",&Val);
+
+    printf("\n\n Character for Given ASCII Value is = %c.", Val);
+
+    getch();
+    return 0;
+
+
+}
