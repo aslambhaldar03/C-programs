cd66d36147c7d448b915a4de2b892e1cc148e360
aslambhaldar03 <86196631+aslambhaldar03@users.noreply.github.com>
2021-06-21 14:43:31 +0530

    Add files via upload

---
+++ Character to ASCII Value.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch = '\0';
+
+    printf("\n Enter a Character to Get its Corresponding ASCII Value = ");
+    scanf("%c",&ch);
+
+    printf("\n\n Character for Given ASCII Value is = %d.", ch);
+
+    getch();
+    return 0;
+}
