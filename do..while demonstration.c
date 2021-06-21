f1f625331c43b2ef742b67198512afb9b645df08
aslambhaldar03 <86196631+aslambhaldar03@users.noreply.github.com>
2021-06-21 14:44:01 +0530

    Add files via upload

---
+++ do..while demonstration.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    do
+    {
+        print("\n INDIA");
+        printf("\n Value of Variable = %d", i);
+        i++;
+    }while(i <= 5);
+
+    printf("\n\n Thanks !!!");
+
+    getch();
+    return 0;
+
+}
